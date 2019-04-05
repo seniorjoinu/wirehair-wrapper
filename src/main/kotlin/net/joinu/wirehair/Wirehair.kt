8ff1634056f117de2ef286bb3b057f0a2fa209c3
Alexander Vtyurin <senior.joinu@gmail.com>
2019-04-05 18:27:09 +0300

    Initial commit

---
+++ src/main/kotlin/net/joinu/wirehair/Wirehair.kt
@@ -0,0 +1,45 @@
+package net.joinu.wirehair
+
+import com.sun.jna.Library
+import com.sun.jna.Native
+import com.sun.jna.Structure
+import com.sun.jna.ptr.IntByReference
+
+
+interface Wirehair : Library {
+    companion object {
+        val LIBRARY_NAME = "wirehair"
+        var WRAPPER: Wirehair
+
+        init {
+            System.setProperty("jna.debug_load.jna", "true")
+            val libFile = Native.extractFromResourcePath(LIBRARY_NAME)
+            WRAPPER = Native.load(libFile.absolutePath, Wirehair::class.java)
+        }
+    }
+
+    fun wirehair_init(): Int
+    fun wirehair_encoder_create(reuseOpt: WirehairCodec?, message: ByteArray, messageBytes: Int, blockBytes: Int): WirehairCodec?
+    fun wirehair_encode(codec: WirehairCodec, blockId: Int, blockDataOut: ByteArray, outBytes: Int, dataBytesOut: IntByReference): Int
+    fun wirehair_decoder_create(reuseOpt: WirehairCodec?, messageBytes: Int, blockBytes: Int): WirehairCodec?
+    fun wirehair_decode(codec: WirehairCodec, blockId: Int, blockData: ByteArray, dataBytes: Int): Int
+    fun wirehair_recover(codec: WirehairCodec, messageOut: ByteArray, messageBytes: Int): Int
+    fun wirehair_recover_block(codec: WirehairCodec, blockId: Int, blockData: ByteArray, bytesOut: IntByReference): Int
+    fun wirehair_decoder_becomes_encoder(codec: WirehairCodec): Int
+    fun wirehair_free(codec: WirehairCodec)
+
+    class WirehairCodec : Structure()
+    object WirehairResult {
+        const val Success = 0
+        const val NeedMore = 1
+        const val InvalidInput = 2
+        const val BadDenseSeed = 3
+        const val BadPeelSeed = 4
+        const val BadInputSmallN = 5
+        const val BadInputLargeN = 6
+        const val ExtraInsufficient = 7
+        const val Error = 8
+        const val OOM = 9
+        const val UnsupportedPlatform = 10
+    }
+}
---
+++ src/test/kotlin/net/joinu/wirehair/ReadmeTest.kt
@@ -0,0 +1,70 @@
+package net.joinu.wirehair
+
+import com.sun.jna.ptr.IntByReference
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
+
+
+object ReadmeTest: Spek({
+    Feature("Wrapper works fine") {
+        Scenario("Repeating the example test from wirehair repo") {
+            val initResult = Wirehair.WRAPPER.wirehair_init()
+
+            assert(initResult == Wirehair.WirehairResult.Success)
+
+            val kPacketSize = 1400
+            val kMessageBytes = 1000 * 1000 + 333
+
+            val message = ByteArray(kMessageBytes)
+
+            val encoder = Wirehair.WRAPPER.wirehair_encoder_create(null, message, kMessageBytes, kPacketSize)
+                ?: error("Encoder is not created")
+
+            val decoder = Wirehair.WRAPPER.wirehair_decoder_create(null, kMessageBytes, kPacketSize)
+
+            if (decoder == null) {
+                Wirehair.WRAPPER.wirehair_free(encoder)
+                error("Decoder is not created")
+            }
+
+            var blockId = 0
+            var needed = 0
+
+            while (true) {
+                blockId++
+
+                if (blockId % 10 == 0) continue
+
+                ++needed
+
+                val block = ByteArray(kPacketSize)
+                val writeLen = 0
+
+                val encodeResult = Wirehair.WRAPPER.wirehair_encode(
+                    encoder,
+                    blockId,
+                    block,
+                    kPacketSize,
+                    IntByReference(writeLen)
+                )
+
+                assert(encodeResult == Wirehair.WirehairResult.Success) { "Encode failed" }
+
+                val decodeResult = Wirehair.WRAPPER.wirehair_decode(decoder, blockId, block, writeLen)
+
+                if (decodeResult == Wirehair.WirehairResult.Success)
+                    break
+
+                assert(decodeResult == Wirehair.WirehairResult.NeedMore) { "Decode failed" }
+            }
+
+            val decoded = ByteArray(kMessageBytes)
+            val recoverResult = Wirehair.WRAPPER.wirehair_recover(decoder, decoded, kMessageBytes)
+
+            assert(recoverResult == Wirehair.WirehairResult.Success) { "Recover failed" }
+
+            Wirehair.WRAPPER.wirehair_free(encoder)
+            Wirehair.WRAPPER.wirehair_free(decoder)
+        }
+    }
+})
